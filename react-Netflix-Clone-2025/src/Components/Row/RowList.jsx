8f7cdb7ca7e63d31c889c732b9ca7e7332609e58
Fevan-sys <a.atsede@yahoo.com>
2025-05-15 15:35:35 -0400

    Row

---
+++ react-Netflix-Clone-2025/src/Components/Row/RowList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RowList() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default RowList
---
+++ react-Netflix-Clone-2025/src/Utils/Requestes.jsx
@@ -0,0 +1,16 @@
+// Use import.meta.env for Vite or process.env for Create React App
+const Api_key = import.meta.env.VITE_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${Api_key}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${Api_key}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${Api_key}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${Api_key}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${Api_key}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${Api_key}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${Api_key}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${Api_key}&with_genres=99`,
+  fetchTvShow: `tv/popular?api_key=${Api_key}&language=en-US&page=1`,
+};
+
+export default requests;
